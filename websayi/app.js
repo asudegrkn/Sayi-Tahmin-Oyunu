bda39d43a1fd75639e57717c4984840433b14811
asudegrkn <asudegr@gmail.com>
2022-04-27 13:41:01 +0300

    create number guessing game with javascript

---
+++ websayi/app.js
@@ -0,0 +1,44 @@
+var randomSayi,
+  sayac,
+  hak = 3;
+var tahmin = document.querySelector("#tahmin");
+var baslat = document.querySelector("#baslat");
+var sayi = document.querySelector("#sayi");
+
+baslat.onclick = function () {
+  tahmin.disabled = false;
+  randomSayi = Math.round(Math.random() * 1000);
+  sayac = 0;
+  alert(randomSayi);
+};
+
+tahmin.onclick = function () {
+  var sayiYuzler = Math.floor(sayi.value / 100);
+  var sayiOnlar = Math.floor(sayi.value / 100 / 10);
+  var sayiBirler = (sayi.value % 100) % 10;
+  var randomSayiYuzler = Math.floor(randomSayi / 100);
+  var randomSayiOnlar = Math.floor(randomSayi / 100 / 10);
+  var randomSayiBirler = (randomSayi % 100) % 10;
+
+  if (sayi.value >= 1000) alert("Maksimum 3 basamaklı sayı giriniz");
+  sayac++;
+  if (randomSayi != sayi.value && sayac > hak) {
+    alert("Hakkınız kalmadı.");
+  }
+  if (randomSayi == sayi.value) {
+    alert("tahmininiz doğru");
+    tahmin.disabled = true;
+    baslat.disabled = false;
+  }
+  else{
+  if (randomSayiYuzler == sayiYuzler) {
+    alert("yüzler basamağı doğru");
+  }
+  if (randomSayiOnlar == sayiOnlar) {
+    alert("onlar basamağı doğru");
+  }
+  if (randomSayiBirler == sayiBirler) {
+    alert("birler basamağı doğru");
+  }
+}
+}
